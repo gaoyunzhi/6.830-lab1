d9d09419b2b30998118fef4c92a855e3d70cf024
Yunzhi Gao <gaoyunzhi@gmail.com>
2013-02-21 11:44:06 -0500

    format

--- src/simpledb/RecordId.java
+++ src/simpledb/RecordId.java
@@ -5,65 +5,73 @@ package simpledb;
  * specific table.
  */
 public class RecordId {
-	
+
 	private final PageId _pid;
 	private final int _tupleno;
 
-    /** Creates a new RecordId refering to the specified PageId and tuple number.
-     * @param pid the pageid of the page on which the tuple resides
-     * @param tupleno the tuple number within the page.
-     */
-    public RecordId(PageId pid, int tupleno) {
-        _pid=pid;
-        _tupleno=tupleno;
-    }
+	/**
+	 * Creates a new RecordId refering to the specified PageId and tuple number.
+	 * 
+	 * @param pid
+	 *            the pageid of the page on which the tuple resides
+	 * @param tupleno
+	 *            the tuple number within the page.
+	 */
+	public RecordId(PageId pid, int tupleno) {
+		_pid = pid;
+		_tupleno = tupleno;
+	}
 
-    /**
-     * @return the tuple number this RecordId references.
-     */
-    public int tupleno() {
-        return _tupleno;
-    }
+	/**
+	 * @return the tuple number this RecordId references.
+	 */
+	public int tupleno() {
+		return _tupleno;
+	}
 
-    /**
-     * @return the page id this RecordId references.
-     */
-    public PageId getPageId() {
-        return _pid;
-    }
-    
-    /* (non-Javadoc)
+	/**
+	 * @return the page id this RecordId references.
+	 */
+	public PageId getPageId() {
+		return _pid;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
-    @Override
-    public boolean equals(Object obj) {
-	    if (this == obj)
-		    return true;
-	    if (obj == null)
-		    return false;
-	    if (getClass() != obj.getClass())
-		    return false;
-	    RecordId other = (RecordId) obj;
-	    if (_pid == null) {
-		    if (other._pid != null)
-			    return false;
-	    } else if (!_pid.equals(other._pid))
-		    return false;
-	    if (_tupleno != other._tupleno)
-		    return false;
-	    return true;
-    }
-    
-    /* (non-Javadoc)
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecordId other = (RecordId) obj;
+		if (_pid == null) {
+			if (other._pid != null)
+				return false;
+		} else if (!_pid.equals(other._pid))
+			return false;
+		if (_tupleno != other._tupleno)
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#hashCode()
 	 */
-    @Override
-    public int hashCode() {
-	    final int prime = 31;
-	    int result = 1;
-	    result = prime * result + ((_pid == null) ? 0 : _pid.hashCode());
-	    result = prime * result + _tupleno;
-	    return result;
-    }
-    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_pid == null) ? 0 : _pid.hashCode());
+		result = prime * result + _tupleno;
+		return result;
+	}
+
 }
